81eb657ce2ae909950f69fde3a7345d56e2ce161
root <01ssrmikururudevice01@gmail.com>
2017-03-26 18:46:32 +0900

    add PNG file

--- crypto/base64decoder.py
+++ crypto/base64decoder.py
@@ -3,6 +3,16 @@
 import sys
 import base64
 
-s = sys.argv[1]
+def main():
+	s = sys.argv[1]
+	file = open(s,'rt').read()
+	decode = base64.b64decode(file)
+	writefile = open('decode_file','w')
+	writefile.write(decode)
 
-print(base64.b64decode(s))
+
+#print(base64.b64decode(s))
+
+
+if __name__ == '__main__':
+	main()
