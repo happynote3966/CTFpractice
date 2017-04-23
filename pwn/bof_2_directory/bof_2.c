a9f6a2d98df7e1bd9c28ff7bd1ffdef5193f3895
root <01ssrmikururudevice01@gmail.com>
2017-04-23 20:53:16 +0900

    add shellscript

--- pwn/bof_2_directory/bof_2.c
+++ pwn/bof_2_directory/bof_2.c
@@ -10,13 +10,13 @@ void input(void){
 	char buf[20] = {0};
 	gets(buf);
 	printf("Hi, %s\n",buf);
+	fflush(stdout);
 }
 
 int main(void){
 	printf("flag_function_address = %p\n",flag);
 	printf("Input your data : ");
 	fflush(stdout);
-	sleep(5);
 	input();
 	return 0;
 }
