281a1609e77d1dab2541da66acc8ea1620bcdf01
root <01ssrmikururudevice01@gmail.com>
2017-08-05 06:43:47 +0900

    add gamestart.sh

---
+++ pwn/gamestart.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+cd bof_0_directory
+./start.sh
+cd ..
+
+cd bof_1_directory
+./start.sh
+cd ..
+
+cd bof_2_directory
+./start.sh
+cd ..
+
+cd bof_3_directory
+./start.sh
+cd ..
+
+cd bof_4_directory
+./start.sh
+cd ..
+
+cd bof_5_directory
+./start.sh
+cd ..
+
+cd bof_6_directory
+./start.sh
+cd ..
